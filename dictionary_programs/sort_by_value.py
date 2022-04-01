1428131c814e3ffca181fee6247f8ecb4dc17866
Nishanth <rajkumarnishanth@gmail.com>
2022-04-01 23:00:52 +0530

    fix: added descending sort

--- dictionary_programs/sort_by_value.py
+++ dictionary_programs/sort_by_value.py
@@ -8,7 +8,7 @@
 import logging
 import sys
 
-def sort_by_value(dictionary_to_sort: dict):
+def sort_by_value(dictionary_to_sort: dict, descending: bool = False):
     """
         Description:
             Sort the given dictionary by value
@@ -19,11 +19,12 @@ def sort_by_value(dictionary_to_sort: dict):
         Return:
             None
     """
-    sorted_dictionary = {key: val for key, val in sorted(dictionary_to_sort.items(), key= lambda item: item[1])}
+    sorted_dictionary = {key: val for key, val in sorted(dictionary_to_sort.items(), key= lambda item: item[1], reverse=descending)}
     logging.info(f"Orginal Dictionary: {dictionary_to_sort}")
-    logging.info(f"Sorted by Value: {sorted_dictionary}")
+    logging.info(f"Sorted by Value(Descending={descending}): {sorted_dictionary}")
 
 if __name__ == '__main__':
     logging.basicConfig(handlers=[logging.FileHandler(r"dictionary_programs\dictionary.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
     sample_dictionary = {1: 230, 2: 350, 3: 130}
     sort_by_value(sample_dictionary)
+    sort_by_value(sample_dictionary, True)
