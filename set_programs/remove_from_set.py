6f8e0e7eb8464ae2a93bea89e70fcac6d6ba7e0b
Nishanth <rajkumarnishanth@gmail.com>
2022-04-03 22:28:33 +0530

    fix: remove multiple items from set

--- set_programs/remove_from_set.py
+++ set_programs/remove_from_set.py
@@ -8,27 +8,24 @@
 import sys
 import logging
 
-def remove(set_data: set, item) -> set:
+def remove(set_data: set, *items) -> set:
     """
         Description:
-            Removes the given item from the set
+            Removes the given item(s) from the set
         
         Parameter:
             set_data: The set to remove the item from
-            item: the item to be removed
+            items: the item(s) to be removed
         
         Return:
             returns a set
     """
-    try:
-        set_data.remove(item)
-        return set_data
-    except KeyError:
-        logging.exception(f"{item} not present in the set: {set_data}")
+    set_of_items = set(items)
+    return set_data.difference(set_of_items)
 
 if __name__=='__main__':
     logging.basicConfig(handlers=[logging.FileHandler(r"set_programs\sets.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
     original_set = set(['asd', 'qwe', 123, 'rty'])
     logging.info(f"Original set: {original_set}")
-    updated_set = remove(original_set, 123)
+    updated_set = remove(original_set, 123, 'asd')
     logging.info(f"Updated set: {updated_set}")
