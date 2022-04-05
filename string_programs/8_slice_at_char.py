c81b964601a768e601b42a32892e20598089f7f9
Nishanth <rajkumarnishanth@gmail.com>
2022-04-05 14:41:38 +0530

    feat: get substring before a specified character

---
+++ string_programs/8_slice_at_char.py
@@ -0,0 +1,32 @@
+'''
+    @Author: Nishanth
+    @Date: 05-04-2022 14:32:00
+    @Last Modified by: Nishanth
+    @Last Modified Date: 05-04-2022 14:32:00
+    @Title: Gets the last part of a string before a specified character
+'''
+import sys
+import logging
+
+def slice_at_char(text: str):
+    """
+        Description:
+            Sorts comma separated words alphanumerically
+        
+        Parameter:
+            comma_sep_text: comma separated text
+        
+        Return:
+            None
+    """
+    logging.info(f"Original text: {text}")
+    split_char = input("Enter a char from above text to get sliced text: ")
+    if len(split_char) != 1:
+        logging.error("Input char must be a single char only")
+        return None
+    text_result = text.split(split_char, maxsplit=1)[0]
+    logging.info(f"Sliced text: {text_result}")
+
+if __name__=='__main__':
+    logging.basicConfig(handlers=[logging.FileHandler(r'string_programs\string.log'), logging.StreamHandler(sys.stdout)], level=logging.INFO)
+    slice_at_char('https://www.w3resource.com/python-exercises')
