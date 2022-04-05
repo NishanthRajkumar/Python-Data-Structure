aa06070590a6fe5cc85153ea5567f9703860b80e
Nishanth <rajkumarnishanth@gmail.com>
2022-04-05 09:26:23 +0530

    feat: remove duplicate lists

---
+++ list_programs/17_duplicate_lists.py
@@ -0,0 +1,35 @@
+'''
+    @Author: Nishanth
+    @Date: 05-04-2022 08:52:00
+    @Last Modified by: Nishanth
+    @Last Modified Date: 05-04-2022 08:52:00
+    @Title: Remove duplicate lists
+'''
+import sys
+import logging
+
+def remove_duplicate_lists(list_of_lists: list[list]):
+    """
+        Description:
+            Remove duplicate lists
+        
+        Parameter:
+            list1, list2: The 2 lists to check if circularly identical
+        
+        Return:
+            None
+    """
+    logging.info(f"Original list: {list_of_lists}")
+    distinct_list_of_lists = []
+    for item in list_of_lists:
+        if type(item) is not list:
+            logging.error("The list must be a list of lists")
+            return None
+        if item not in distinct_list_of_lists:
+            distinct_list_of_lists.append(item)
+    logging.info(f"Distinct list: {distinct_list_of_lists}")
+
+if __name__=='__main__':
+    logging.basicConfig(handlers=[logging.FileHandler(r"list_programs\list_programs.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
+    sample_list = [[10, 20], [40], [30, 56, 25], [10, 20], [33], [40]]
+    remove_duplicate_lists(sample_list)
