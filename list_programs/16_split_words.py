557f8655b33ee8b8b0598c65840d3f55f4757622
Nishanth <rajkumarnishanth@gmail.com>
2022-04-05 10:56:16 +0530

    feat: split list based on 1st character of word

---
+++ list_programs/16_split_words.py
@@ -0,0 +1,31 @@
+'''
+    @Author: Nishanth
+    @Date: 05-04-2022 10:35:00
+    @Last Modified by: Nishanth
+    @Last Modified Date: 05-04-2022 10:35:00
+    @Title: Split a list based on first character of word
+'''
+import sys
+import logging
+
+def remove_items(list_data: list):
+    """
+        Description:
+            Splits a list based on first character of word
+        
+        Parameter:
+            list_data: list of data
+        
+        Return:
+            None
+    """
+    logging.info(f"Original list: {list_data}")
+    for i in range(len(list_data)):
+        if type(list_data[i]) is str and len(list_data[i]) > 1:
+            list_data[i] = [list_data[i][0], list_data[i][1:]]
+    logging.info(f"Updated list: {list_data}")
+
+if __name__=='__main__':
+    logging.basicConfig(handlers=[logging.FileHandler(r"list_programs\list_programs.log"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
+    sample_list = ['Red', 'Green', 'White', 234, 3+8j, 'Black', 'Pink', 'Yellow']
+    remove_items(sample_list)
